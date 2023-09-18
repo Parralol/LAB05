3e18d38d855bea0b4d7e344d8d4f47c10b3eb00d
Parralol <110953563+Parralol@users.noreply.github.com>
2023-09-17 21:23:00 -0500

    Add files via upload

--- demoFuncional/src/main/java/com/example/servingwebcontent/UserController.java
+++ demoFuncional/src/main/java/com/example/servingwebcontent/UserController.java
@@ -12,9 +12,15 @@ public class UserController {
     @GetMapping("/user")
     public ResponseEntity<User> getUser(){
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<User> user = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/todos/1", User.class);
-        System.out.println("User: " + user);
+        ResponseEntity<User> user1 = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/todos/1", User.class);
+        User user = user1.getBody();
+        System.out.println("User: " + user1);
+        System.out.println("UserTitle: " +user.getTitle());
+        System.out.println("id: " +user.getId());
+        System.out.println("Userid: " +user.getUserId());
+        System.out.println("UserClass: " +user.getClass());
+        System.out.println("Completed: " +user.getCompleted());
 
-        return user;
+        return user1;
     }
 }
